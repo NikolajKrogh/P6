e30617c519c1535854e3f1497c14537a01bffabf
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-02-23 11:41:07 +0100

    started work on knn and cleaned up k-means

--- Python/K-means.py
+++ Python/K-means.py
@@ -1,18 +1,14 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 
 from sklearn.cluster import KMeans
-from sklearn import datasets
-from sklearn.utils import shuffle
-from sklearn.datasets import make_blobs
+
 
 
 # import some data to play with
 data = pd.read_csv('mhealth_raw_data.csv')
-X = np.array([data.alx.values, data.alz.values]) 
-y = data.aly.values
+X = np.array([data.grx.values, data.gry.values]) 
 newX = []
 
 i=0
@@ -21,13 +17,12 @@ while(i < len(X[0])):
     i += 1
 print(newX)
 newX = np.array(newX)
-plt.scatter(X[:, 0], X[:, 1], s=50);
 
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(newX)
+kmeans = KMeans(n_clusters=2, random_state=42, n_init='auto')
+kmeans.fit_transform(newX)
 y_kmeans = kmeans.predict(newX)
-
-plt.scatter(newX[:, 0], newX[:, 1], c=y_kmeans, s=50, cmap='viridis')
+print(y_kmeans)
+plt.scatter(newX[:, 0], newX[:, 1], c=y_kmeans, cmap='viridis')
 
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
