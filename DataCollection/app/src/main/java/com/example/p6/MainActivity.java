9fbb5e8fe14eed3d93da579b456d83fe05534e9c
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-02-27 12:21:21 +0100

    Heart rate and accelerometer works and prints on the watch

--- DataCollection/app/src/main/java/com/example/p6/MainActivity.java
+++ DataCollection/app/src/main/java/com/example/p6/MainActivity.java
@@ -5,27 +5,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.example.p6.databinding.ActivityMainBinding;
 
-import java.util.List;
-
-public class MainActivity extends Activity implements SensorEventListener {
+public abstract class MainActivity extends Activity implements SensorEventListener {
 
     SensorManager mSensorManager;
-    Sensor accSensor;
+    private Sensor senAccelerometer;
+    private Sensor senHeartRateCounter;
+    private String heartRate;
     String x_axis;
     String y_axis;
     String z_axis;
 
-    private TextView statusLabel;
+    private TextView accelerometerText;
+    private TextView heartRateText;
+
     private ActivityMainBinding binding;
 
     @Override
@@ -34,31 +34,39 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senHeartRateCounter = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
 
-        //mSensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        mSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, senHeartRateCounter, SensorManager.SENSOR_DELAY_FASTEST);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        statusLabel = binding.statusLabel;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor arg0, int arg1) {
-
+        accelerometerText = binding.accelerometerText;
+        heartRateText = binding.heartRateText;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        x_axis = Float.toString(event.values[0]);
-        y_axis = Float.toString(event.values[1]);
-        z_axis = Float.toString(event.values[2]);
-        statusLabel.setText(x_axis + ", " + y_axis + ", " + z_axis);
-        //Log.i("x,y,z", x_axis + ", " + y_axis + ", " + z_axis);
+        if (event.sensor.getType()==Sensor.TYPE_HEART_RATE){
+            heartRate = Float.toString(event.values[0]);
+            Log.i("Heart Rate:", heartRate);
+            heartRateText.setText("Heart Rate:" + heartRate);
+        }
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            x_axis = Float.toString(event.values[0]);
+            y_axis = Float.toString(event.values[1]);
+            z_axis = Float.toString(event.values[2]);
+            accelerometerText.setText("accelerometer:" +x_axis + "x: , " + y_axis + "y: , " + z_axis + "z: ");
+            //Log.i("x,y,z", x_axis + ", " + y_axis + ", " + z_axis);
+        }
     }
 
     public void onButtonClick(View view) {
         Log.i("Sensors", mSensorManager.getSensorList(Sensor.TYPE_ALL).toString());
     }
 }
+
+
+
