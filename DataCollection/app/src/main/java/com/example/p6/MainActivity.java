303675c2c2f8bf70a55bb4f40fae125117a30d63
KasperBahrentz <74188293+KasperBahrentzAAU@users.noreply.github.com>
2023-03-02 12:01:26 +0100

    Heartrate and accelerometer data are now included in the same datapoint

--- DataCollection/app/src/main/java/com/example/p6/MainActivity.java
+++ DataCollection/app/src/main/java/com/example/p6/MainActivity.java
@@ -26,26 +26,26 @@ import java.util.List;
 public class MainActivity extends Activity implements SensorEventListener {
     class Row
     {
-        long timestamp;
+        String timestamp;
         String heartbeat;
         String acc_x;
         String acc_y;
         String acc_z;
         String label;
 
-        public Row(long timestamp, String label)
+        public Row(String timestamp, String heartbeat, String acc_x, String acc_y, String acc_z, String label)
         {
             this.timestamp = timestamp;
+            this.heartbeat = heartbeat;
             this.label = label;
-        }
-        public String[] toList()
-        {
-            return new String[] {Long.toString(timestamp),heartbeat,acc_x,acc_y,acc_z,label};
+            this.acc_x = acc_x;
+            this.acc_y = acc_y;
+            this.acc_z = acc_z;
         }
 
         public String toString()
         {
-            return String.format("%s,%s,%s,%s,%s,%s\n", Long.toString(timestamp),heartbeat,acc_x,acc_y,acc_z,label);
+            return String.format("%s,%s,%s,%s,%s,%s\n", timestamp,heartbeat,acc_x,acc_y,acc_z,label);
         }
 
 
@@ -55,12 +55,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     SensorManager mSensorManager;
     Sensor senAccelerometer;
     private Sensor senHeartRateCounter;
-    private String heartRate;
-    String x_axis;
-    String y_axis;
-    String z_axis;
-    long firstTime = 0;
-    int i = -1000;
+    float heartRate = 0;
     boolean hasGotHeartBeatData = false;
 
     private TextView accelerometerText;
@@ -86,74 +81,47 @@ public class MainActivity extends Activity implements SensorEventListener {
         heartRateText = binding.heartRateText;
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor arg0, int arg1) {
-
-    }
-
-
-    public void insertAccelerometerDataAtTimestamp(float[] accelerometerEventValues, long timestamp , List<Row> rows)
+    public void insertDataAtTimeStamp(long timestamp, float heartRate, float acc_x, float acc_y, float acc_z , List<Row> rows)
     {
-        for (Row row: rows) {
-            if (row.timestamp == timestamp)
-            {
-                row.acc_x = Float.toString(accelerometerEventValues[0]);
-                row.acc_y = Float.toString(accelerometerEventValues[1]);
-                row.acc_z = Float.toString(accelerometerEventValues[2]);
-            }
-        }
-    }
+        int label = 0; //0 for walking, 1 for running
+        Row row = new Row(
+                Long.toString(timestamp),
+                Float.toString(heartRate),
+                Float.toString(acc_x),
+                Float.toString(acc_y),
+                Float.toString(acc_z),
+                Integer.toString(label)
+        );
 
-    private void insertHeartbeatDataAtTimestamp(float heartbeat, long timestamp , List<Row> rows)
-    {
-        for (Row row: rows) {
-            if (row.timestamp == timestamp)
-            {
-                row.heartbeat = Float.toString(heartbeat);
-            }
-        }
+        rows.add(row);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        long time = event.timestamp;
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            heartRate = event.values[0];
+            heartRateText.setText("Heart Rate:" + heartRate);
+            hasGotHeartBeatData = true;
+        }
+        if (hasGotHeartBeatData && event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x_axis = event.values[0];
+            float y_axis = event.values[1];
+            float z_axis = event.values[2];
+            insertDataAtTimeStamp(time, heartRate, x_axis, y_axis, z_axis, rows);
+            accelerometerText.setText("accelerometer: x: " + x_axis + ", y: " + y_axis + ", z: " + z_axis);
+            hasGotHeartBeatData = false;
+        }
+    }
 
-        if (i >= 100 || event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-            if (firstTime == 0)
-                firstTime = event.timestamp;
-            long time = event.timestamp;
-            int label = 0; //0 for walking, 1 for running
-            rows.add(new Row(time, Integer.toString(label)));
-            if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-                insertHeartbeatDataAtTimestamp(event.values[0], time, rows);
-                heartRate = Float.toString(event.values[0]);
-                Log.i("Heart Rate:", heartRate);
-                heartRateText.setText("Heart Rate:" + heartRate);
-                hasGotHeartBeatData = true;
-            }
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                insertAccelerometerDataAtTimestamp(event.values, time, rows);
-                x_axis = Float.toString(event.values[0]);
-                y_axis = Float.toString(event.values[1]);
-                z_axis = Float.toString(event.values[2]);
-                accelerometerText.setText("accelerometer: x: " + x_axis + ", y: " + y_axis + ", z: " + z_axis);
-                //Log.i("x,y,z", x_axis + ", " + y_axis + ", " + z_axis);
-            }
-            if (hasGotHeartBeatData){
-                i = 100;
-                hasGotHeartBeatData = false;
-            }
-            else {
-                i = 0;
-            }
-        }
-        else {
-            i++;
-        }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     public void onButtonClick(View view) {
         //String[] header = { "timestamp,heartbeat,acc_x,acc_y,acc_z,label"};
-        String finalString = "timestamp,heartbeat,acc_x,acc_y,acc_z,label\n";
+        String finalString = "timestamp,heartrate,acc_x,acc_y,acc_z,label\n";
 
         for (Row row: rows)
         {
