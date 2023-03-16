f75a5b4db5818fbd3236f8928db095d3796453e4
lildhansen <madslh@yahoo.com>
2023-03-16 15:32:42 +0100

    budget timeseries virker med nearest centroid. det er ultra ulæseligt og sådan, og der mangler også mere data, men kinda det må fremtidige mig se på

--- DataClassification/k-nearest_neighbor.py
+++ DataClassification/k-nearest_neighbor.py
@@ -12,9 +12,14 @@ from sklearn.neighbors import NearestCentroid
 pamapData = pd.read_csv(r"data\pamapdataWithHR.csv")
 recordedWalkingData = pd.read_csv(r"data\walking_2023-03-02_16_43_58.csv")
 ourWalking = pd.read_csv(r"data\ourData\walking_2023-03-10_13_54_52.csv")
+ourWalking2 = pd.read_csv(r"data\ourData\walking_2023-03-16_12_30_34.csv")
 ourCycling = pd.read_csv(r"data\ourData\cycling_2023-03-11_11_22_01.csv")
 ourRunning = pd.read_csv(r"data\ourData\running_2023-03-12_06_10_19.csv")
+ourRunning2 = pd.read_csv(r"data\ourData\running_2023-03-16_12_48_25.csv")
 ourIdle = pd.read_csv(r"data\ourData\idle_2023-03-10_11_33_21.csv")
+ourIdle2 = pd.read_csv(r"data\ourData\idle_2023-03-16_10_59_38.csv")
+ourIdle3 = pd.read_csv(r"data\ourData\idle_2023-03-16_13_00_55.csv")
+test = pd.read_csv(r"data\ourData\test.csv")
 
 own_heartrate = "heart_rate"
 own_x_accelerometer = "acc_x"
@@ -22,6 +27,7 @@ own_y_accelerometer = "acc_y"
 own_z_accelerometer = "acc_z"
 ownLabel = "label"
 own_timestamp = "timestamp"
+minute_timestamp = "minutes"
 NANOSEC_TO_MINUTE_FACTOR = 60000000000
 
 def resetNumpyArrays(accXData,accYData,accZData,heartRateData):
@@ -31,42 +37,148 @@ def resetNumpyArrays(accXData,accYData,accZData,heartRateData):
     heartRateData = np.array([])
 
 def makeTimeSeries(file):
-    hstacks = np.empty([])
+    #file.replace(to_replace=pd.NA, value=0, inplace=True)
+    #file.replace(to_replace=None, value=0, inplace=True)
+    maxMinute = file.loc[:,minute_timestamp+1].max()
+    hstacks = []
+    labels = []
     firstTimestamp = -1
     accXData = None
     accYData = None
     accZData = None
     heartRateData = None
     resetNumpyArrays(accXData,accYData,accZData,heartRateData)
-    i = 0
-    for index,row in file.iterrows():
-        accXData = np.append(accXData,row[own_x_accelerometer])
-        accYData = np.append(accYData,row[own_y_accelerometer])
-        accZData = np.append(accZData,row[own_z_accelerometer])
-        heartRateData = np.append(heartRateData,row[own_heartrate])       
-        timestamp = row[own_timestamp]
-        if firstTimestamp == -1:
-            firstTimestamp = timestamp
-        elif timestamp - firstTimestamp >= 1 * NANOSEC_TO_MINUTE_FACTOR:
-            hstacks = np.append(hstacks,np.hstack((accXData,accYData,accZData,heartRateData)))
-            resetNumpyArrays(accXData,accYData,accZData,heartRateData)
-            i+=1
-    return hstacks
+    maxMinute = file.loc[:,minute_timestamp].max()  #file[file[minute_timestamp]].max()
+    for i in range(maxMinute+1):
+        #try:
+        subFile = file[(file[minute_timestamp]==i)]
+        accXData = subFile.loc[:,own_x_accelerometer]
+        accYData = subFile.loc[:,own_y_accelerometer]
+        accZData = subFile.loc[:,own_z_accelerometer]
+        heartRateData = subFile.loc[:,own_heartrate]
+        labelData = subFile.loc[:,ownLabel]
+        #except KeyError:
+        #    continue
+        hstacks.append(np.hstack((accXData,accYData,accZData,heartRateData)))
+        labels.append(labelData)
+        #hstacks = np.stack((hstacks,np.hstack((accXData,accYData,accZData,saa  heartRateData))))
+        #hstacks[i] = np.hstack((accXData,accYData,accZData,heartRateData))
+        #print(hstacks)
+        #labels = np.stack((labels,labelData))
+        resetNumpyArrays(accXData,accYData,accZData,heartRateData)
+    #print(hstacks)
+    return hstacks, labels
 
-ourIdleX = makeTimeSeries(ourIdle)
-ourIdleY = np.empty(len(ourIdleX)); ourIdleY.fill(0) #idle label = 0
+def getRows(file):
+    result = 0
+    for i in range(4):
+        subFile = file[(file[ownLabel]==i)]
+        if np.isnan(subFile.loc[:,minute_timestamp].max()):
+            continue
+        result += subFile.loc[:,minute_timestamp].max()
+    return result 
+
+    
+def makeBudgetTimeSeries(file):
+    firstRun = True
+    rowCount = getRows(file)
+    rows = np.empty((rowCount+1,4))
+    rows[0] = [1,2,3,4]
+    labels = np.empty((rowCount+1,1))
+    #label = file[ownLabel].iloc[0]
+    accXMean = None
+    accYMean = None
+    accZMean = None
+    heartRateMean = None
+    resetNumpyArrays(accXMean,accYMean,accZMean,heartRateMean)
+    maxMinute = file.loc[:,minute_timestamp].max() 
+    counter = 0
+    for i in range(4):
+        subFile = file[(file[ownLabel]==i)]
+        for j in range(maxMinute+1):
+            subSubFile = subFile[(file[minute_timestamp]==j)]
+            accXMean = subSubFile.loc[:,own_x_accelerometer].abs().mean()
+            accYMean = subSubFile.loc[:,own_y_accelerometer].abs().mean()
+            accZMean = subSubFile.loc[:,own_z_accelerometer].abs().mean()
+            heartRateMean = subSubFile.loc[:,own_heartrate].mean()
+            if (np.isnan(accXMean)):
+                continue
+            # if firstRun:
+            #     rows = np.array([accXMean,accYMean,accZMean,heartRateMean])
+            #     labels = np.array([i])
+            #     firstRun = False
+            #     continue
+            #print("rows:",rows)
+            #print("next array: ",np.array([accXMean,accYMean,accZMean,heartRateMean]))
+            rows[counter] = np.array([accXMean,accYMean,accZMean,heartRateMean])
+            labels[counter] = np.array([i])
+            
+            counter += 1
+            
+            
+            #rows = np.append(rows,np.array([accXMean,accYMean,accZMean,heartRateMean]), axis=0)
+            #print(rows)
+            #rows[i] = np.array([accXMean,accYMean,accZMean,heartRateMean])
+            #labels = np.append(labels,np.array([i]), axis=0)
+            #labels[i] = label
+    return rows,labels
+
+
+testX,testY = makeBudgetTimeSeries(test)
+X_train, X_test, y_train, y_test = train_test_split(testX, testY, test_size = 0.5, random_state=42)
+
+#ourIdleX,ourIdleY = makeBudgetTimeSeries(ourIdle)
+#ourIdleX2,ourIdleY2 = makeBudgetTimeSeries(ourIdle2)
+#ourWalkingX,ourWalkingY = makeBudgetTimeSeries(ourWalking2)
+
+#print("running:")
+#ourRunningX,ourRunningY = makeBudgetTimeSeries(ourRunning2)
+
+knn = NearestCentroid()
+
+#print("x:",testX)
+#print("y:",testY)
+knn.fit(X_train, np.ravel(y_train,order='C'))
+#knn.fit(ourWalkingX,np.ravel(ourWalkingY,order='C'))
+
+print("accuracy:", accuracy_score(knn.predict(X_test),np.ravel(y_test,order='C')))
+
+quit()
+    # for index,row in file.iterrows():
+    #     accXData = np.append(accXData,row[own_x_accelerometer])
+    #     accYData = np.append(accYData,row[own_y_accelerometer])
+    #     accZData = np.append(accZData,row[own_z_accelerometer])
+    #     heartRateData = np.append(heartRateData,row[own_heartrate])       
+    #     timestamp = row[own_timestamp]
+    #     if firstTimestamp == -1:
+    #         firstTimestamp = timestamp
+    #     elif timestamp - firstTimestamp >= 1 * NANOSEC_TO_MINUTE_FACTOR:
+    #         hstacks = np.append(hstacks,np.hstack((accXData,accYData,accZData,heartRateData)))
+    #         resetNumpyArrays(accXData,accYData,accZData,heartRateData)
+    #         i+=1
+    # return hstacks
+
+ourIdleX,ourIdleY = makeTimeSeries(ourIdle)
+ourIdleX2,ourIdleY2 = makeTimeSeries(ourIdle2)
+# ourIdleY = np.empty(len(ourIdleX)); ourIdleY.fill(0) #idle label = 0
 # ourWalkingX = makeTimeSeries(ourWalking)
 # ourWalkingY = np.empty(len(ourWalkingX)); ourWalkingY.fill(1)  #walking label = 1
-# ourRunningX = makeTimeSeries(ourRunning)
-# ourRunningY = np.empty(len(ourRunningX)); ourRunningY.fill(2)  #running label = 2
+#ourRunningX,ourRunningY = makeTimeSeries(ourRunning)
+#ourRunningY = np.empty(len(ourRunningX)); ourRunningY.fill(2)  #running label = 2
 # ourCyclingX = makeTimeSeries(ourCycling)
 # ourCyclingY = np.empty(len(ourCyclingX)); ourCyclingY.fill(3)  #cycling label = 3
 
-print(type(ourIdleX))
 
-knn = KNeighborsClassifier(metric=dtw,n_neighbors=400)
+
+knn = KNeighborsClassifier(metric=dtw,n_neighbors=4)
+
+print(type(ourIdleY))
+#ourIdleX = np.reshape(ourIdleX,(-1,1))
+
+print(ourIdleX)
 knn.fit(ourIdleX, ourIdleY)
 
+#print("Walking dataset:", accuracy_score(knn.predict(ourIdleX2),ourIdleY2))
 
             
 
