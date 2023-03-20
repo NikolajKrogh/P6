3af3263cbf83beacccb084ed553e2fbc56d73835
KasperBahrentz <74188293+KasperBahrentzAAU@users.noreply.github.com>
2023-03-20 11:38:36 +0100

    If DisplayActivity is already running, then redirect to that activity

--- DataCollection/app/src/main/java/com/example/p6/SelectActivity.java
+++ DataCollection/app/src/main/java/com/example/p6/SelectActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.p6.databinding.ActivitySelectBinding;
 
 public class SelectActivity extends Activity {
 
@@ -18,19 +21,26 @@ public class SelectActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        if(!isTaskRoot()){  // If DisplayActivity is already running, redirect to that
+            startDisplayActivity();
+        }
+        else {
+            super.onCreate(savedInstanceState);
+            ActivitySelectBinding binding = ActivitySelectBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+        }
+    }
+
+    public void startDisplayActivity(){
+        Intent intent = new Intent(SelectActivity.this, DisplayActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("activityToTrack", activityToTrack.ordinal());
+        startActivity(intent);
+        finish();
     }
 
     public void onStartButtonClick(View view){
-        // Launch new activity and clear old from history
-        Intent intent = new Intent(SelectActivity.this, DisplayActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        // Send activityToTrack to next activity
-        intent.putExtra("activityToTrack", activityToTrack.ordinal());
-
-        startActivity(intent);
-        finish();
+        startDisplayActivity();
     }
 
     public void onRadioButtonIdle(View view) {
@@ -47,7 +57,7 @@ public class SelectActivity extends Activity {
     }
 
     public void onExitButtonClick(View view) {
-        finish();
+        finishAndRemoveTask();
         System.exit(0);
     }
 }
