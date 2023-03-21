97158b50c4364b43c294c58e5202c3ec5849dcbb
lildhansen <madslh@yahoo.com>
2023-03-21 12:51:56 +0100

    cleaned up preproccessing file such that save_csv is in a seperate function, and a dataframe is passed between the functions

--- DataClassification/preprocessing.py
+++ DataClassification/preprocessing.py
@@ -23,12 +23,13 @@ label = "label"
 #endregion
 
 def add_minute_column(filepath):
+    print(filepath)
     first_run = True
     minute_counter = 0
     first_timestamp = -1
     file = pd.read_csv(filepath)
     if minute_timestamp in file.columns:
-        return
+        return file
     for index,row in file.iterrows():       
         timestamp_nano_sec = row[timestamp]
         if first_timestamp == -1:
@@ -43,25 +44,25 @@ def add_minute_column(filepath):
                 first_run = False
             first_timestamp = timestamp_nano_sec
             minute_counter += 1
-    file = file[[timestamp,minute_timestamp,heartrate,x_accelerometer,y_accelerometer,
-                 z_accelerometer,step_count,label]]
-    file.dropna(inplace = True)
-    file.to_csv(filepath)
+    return file
 
-def add_session_id(filepath):
+def add_session_id(file):
     session_id_value = random.randrange(0,1000000)
-    file = pd.read_csv(filepath)
     if session_id in file.columns:
-        return 
+        return file
     file[session_id] = session_id_value
+    return file
+
+def save_csv(file,filepath):
+    file.dropna(inplace = True)
     file = file[[timestamp,minute_timestamp,heartrate,x_accelerometer,y_accelerometer,
                  z_accelerometer,step_count,session_id,label]]
     file.to_csv(filepath)
 
-
 for file in os.listdir(path):
     filepath = os.path.join(path, file)
     if file.endswith('.csv'):
-        add_minute_column(filepath)
-        add_session_id(filepath)
+        data_frame = add_minute_column(filepath)
+        data_frame = add_session_id(data_frame)
+        save_csv(data_frame, filepath)
         
