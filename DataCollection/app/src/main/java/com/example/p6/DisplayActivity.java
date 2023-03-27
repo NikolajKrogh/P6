7efd31975ae95307562857595ba12c469895d97b
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-03-27 10:07:17 +0200

    Added new files

--- DataCollection/app/src/main/java/com/example/p6/DisplayActivity.java
+++ DataCollection/app/src/main/java/com/example/p6/DisplayActivity.java
@@ -31,34 +31,6 @@ import java.util.List;
 
 public class DisplayActivity extends Activity implements SensorEventListener, View.OnLongClickListener, View.OnClickListener {
 
-    static class Row{
-        String timestamp;
-        String minutes;
-        String heartRate;
-        String acc_x;
-        String acc_y;
-        String acc_z;
-        String step_count;
-        String label;
-
-        public Row(String timestamp, String minutes, String heart_rate, String acc_x, String acc_y, String acc_z, String step_count, String label)
-        {
-            this.timestamp = timestamp;
-            this.minutes = minutes;
-            this.heartRate = heart_rate;
-            this.acc_x = acc_x;
-            this.acc_y = acc_y;
-            this.acc_z = acc_z;
-            this.step_count = step_count;
-            this.label = label;
-        }
-        @NonNull
-        @Override
-        public String toString(){
-            return String.format("%s,%s,%s,%s,%s,%s,%s,%s\n", timestamp,minutes, heartRate,
-                    acc_x,acc_y,acc_z,step_count,label);
-        }
-    }
 
     enum Time {
         MINUTES,
