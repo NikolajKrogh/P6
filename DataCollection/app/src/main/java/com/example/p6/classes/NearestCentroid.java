483d5c6c90308a762c3edd60d0f2ebc73404ec1f
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-04-05 14:44:13 +0200

    os path

--- DataCollection/app/src/main/java/com/example/p6/classes/NearestCentroid.java
+++ DataCollection/app/src/main/java/com/example/p6/classes/NearestCentroid.java
@@ -1,7 +1,6 @@
 package com.example.p6.classes;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
@@ -36,8 +35,10 @@ public class NearestCentroid {
     }
 
     public void getCentroidsFromFile(Context context) throws IOException, CsvValidationException {
-        String fileName = "centroids.csv";
-        String filePath = context.getFilesDir() + "/" + fileName;
+        File fileName = new File("centroids.csv");
+        File filePath = new File(context.getFilesDir(), String.valueOf(fileName));
+        //String fileName = "centroids.csv";
+        //String filePath = context.getFilesDir() + "/" + fileName;
 
         try {
             FileReader filereader = new FileReader(filePath);
