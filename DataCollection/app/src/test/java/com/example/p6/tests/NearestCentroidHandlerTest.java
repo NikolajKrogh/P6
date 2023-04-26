7ea5785385b1920ef8a7530aca536ebf32f3e8d3
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2023-04-26 20:53:55 +0200

    updateModel test

--- DataCollection/app/src/test/java/com/example/p6/tests/NearestCentroidHandlerTest.java
+++ DataCollection/app/src/test/java/com/example/p6/tests/NearestCentroidHandlerTest.java
@@ -2,7 +2,9 @@ package com.example.p6.tests;
 
 import static org.junit.Assert.*;
 
+import com.example.p6.classes.Centroid;
 import com.example.p6.classes.Constants;
+import com.example.p6.classes.DataPointAggregated;
 import com.example.p6.handlers.NearestCentroidHandler;
 
 import org.junit.Test;
@@ -58,4 +60,22 @@ public class NearestCentroidHandlerTest {
             assertEquals(Constants.Activity.CYCLING, predictedActivity);
         }
     }
+
+    @Test
+    public void updateCentroidWithNewDataPoint_noError() {
+        int length = NearestCentroidHandler.centroids.length;
+        DataPointAggregated input = new DataPointAggregated(1, 1, 1, 1);
+        NearestCentroidHandler.centroids[0] = new Centroid(10,10, 10,10,10,10,(byte) 0,1);
+        NearestCentroidHandler.centroids[1] = new Centroid(5,5, 5,5,5,5,(byte) 0,1);
+        NearestCentroidHandler.centroids[2] = new Centroid(2,2, 2,2,2,2,(byte) 0,1);
+        NearestCentroidHandler.centroids[3] = new Centroid(0,0, 0,0,0,0,(byte) 0,0);
+
+        double[] expected = {5.5,4,3.25,2.8};
+
+        for(int i = 0; i < length; i++){
+            Centroid actual = NearestCentroidHandler.updateModel(Constants.Activity.SITTING, input);
+            assertEquals(expected[i], actual.heartRate, TestingVariables.DELTA);
+        }
+    }
+
 }
