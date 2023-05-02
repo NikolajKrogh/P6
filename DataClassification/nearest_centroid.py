1f22792a0386774e9b3493e7f9802619e1c6263c
lildhansen <madslh@yahoo.com>
2023-05-02 12:33:49 +0200

    made sure that it uses the min/max heart rate data from the aggregated data points rather than the raw data points

--- DataClassification/nearest_centroid.py
+++ DataClassification/nearest_centroid.py
@@ -98,16 +98,22 @@ def add_min_max_step_count_data_to_centroids(centroids, X, y):
         labels = y[np.where(y==i)]
         data_points = X[offset:offset + len(labels)]
         offset += len(labels)
-        #axis=1 means we compare all values in a column
-        centroids[i].max_step_count = data_points.max(axis=1)[1]
-        centroids[i].min_step_count = data_points.min(axis=1)[1]
+        #axis=0 means we compare all values in a column
+        centroids[i].max_step_count = data_points.max(axis=0)[1]
+        centroids[i].min_step_count = data_points.min(axis=0)[1]
         
 #Here the min and max value is added from the raw datapoints (data)
-def add_min_max_heart_rate_data_to_centroids(centroids,data):
-    for label in range(NUMBER_OF_LABELS):
-        heart_rate_data_frame_with_label = get_data_frame_with_label(data,label)[heartrate_as_string]
-        centroids[label].max_heart_rate = heart_rate_data_frame_with_label.max()
-        centroids[label].min_heart_rate = heart_rate_data_frame_with_label.min()
+def add_min_max_heart_rate_data_to_centroids(centroids,X,y):
+    offset = 0
+    for i in range(NUMBER_OF_LABELS):
+        labels = y[np.where(y==i)]
+        data_points = X[offset:offset + len(labels)]
+        print(len(labels),len(data_points))
+        print(data_points.max(axis=0))
+        offset += len(labels)
+        #axis=0 means we compare all values in a column
+        centroids[i].max_heart_rate = data_points.max(axis=0)[0]
+        centroids[i].min_heart_rate = data_points.min(axis=0)[0]
         
 def format_final_centroid_to_java(centroids):
     result = "{"
@@ -132,12 +138,13 @@ if __name__ == '__main__':
     X,y = make_aggregated_time_series(data)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=42)
     nearest_centroid = NearestCentroid() 
-    nearest_centroid.fit(X_train, np.ravel(y_train))
+    nearest_centroid.fit(X, np.ravel(y))
     centroids = convert_scikit_centroids_to_our_centroids(nearest_centroid.centroids_)
     add_min_max_step_count_data_to_centroids(centroids,X,y)
-    add_min_max_heart_rate_data_to_centroids(centroids, data)
+    add_min_max_heart_rate_data_to_centroids(centroids, X,y)
+    
 
     pyperclip.copy(format_final_centroid_to_java(centroids))
    
-    print("accuracy:", accuracy_score(nearest_centroid.predict(X_test),np.ravel(y_test)))
+    #print("accuracy:", accuracy_score(nearest_centroid.predict(X_test),np.ravel(y_test)))
     
